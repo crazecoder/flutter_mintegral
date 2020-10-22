e825ce2d8ed7fae4627ffea90eb7520ff882a944
crazecoder <21527312@qq.com>
2020-10-22 13:19:12 +0800

    test

--- test/flutter_mintegral_test.dart
+++ test/flutter_mintegral_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_mintegral/flutter_mintegral.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_mintegral');
@@ -16,5 +15,5 @@ void main() {
   tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
-  
+
 }
